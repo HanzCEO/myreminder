22019e42537c4d12d5b935f3f7c691f54a77d273
Hanz <hanz@godot.id>
2023-05-01 21:40:13 +0700

    feat: Added `/habits` command

--- index.js
+++ index.js
@@ -17,18 +17,57 @@ const teleapi = axios.create({
 	}
 });
 const app = express();
+const session = { stage: 'menu' };
 
 app.use(express.json());
 
 app.post('/bot', async (req, res) => {
-	await teleapi.post('/sendMessage', {
-		chat_id: req.body.message.from.id,
-		text: JSON.stringify(req.body, null, 4)
-	});
+	await stages[session.stage](req.body);
 	
 	res.header('Content-Type', 'text/html');
 	res.status(200).end();
 });
+
+const stages = {
+	'menu': menuStage
+};
+
+function getSender(update) {
+	return update.message.from;
+}
+
+function getMessageText(update) {
+	return update.message.text;
+}
+
+async function sendMessage(chatId, msg) {
+	await teleapi.post('/sendMessage', {
+		chat_id: chatId,
+		text: msg
+	});
+}
+
+async function menuStage(update) {
+	const txt = getMessageText(update);
+	const conn = pool.promise();
+	
+	if (txt == "/start") {
+		await sendMessage(
+			getSender(update).id,
+			"Hello! you can start by using /newhabit\n" +
+			"and list your habits using /habits command."
+		);
+	}
+
+	if (txt == "/habits") {
+		let [habits] = await conn.query("SELECT id, activity FROM habits ORDER BY id");
+		await sendMessage(
+			getSender(update).id,
+			"Habits\n\n" +
+			habits.map(h => `${h.id} - ${h.activity}`).join('\n')
+		);
+	}
+}
 
 app.listen(process.env.MYRMD_NET_PORT || 8080, () => {
 	console.log("[i] Ran at", process.env.MYRMD_NET_PORT || 8080);
