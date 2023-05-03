1045650df89cd4bad7e0241e0c118ef5ebaa43e3
Hanz <hanz@godot.id>
2023-05-03 07:50:29 +0700

    feat: Added setnewreminder stage

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ app.post('/bot', async (req, res) => {
 
 const stages = {
 	'menu': menuStage,
-	'newhabit': newhabitStage
+	'newhabit': newhabitStage,
+	'setnewreminder': setnewreminderStage
 };
 
 function getSender(update) {
@@ -77,6 +78,14 @@ async function menuStage(update) {
 			"What is the name of the activity?"
 		);
 	}
+
+	if (txt == "/remind") {
+		session.stage = "setnewreminder";
+		await sendMessage(
+			getSender(update).id,
+			"Send your habit ID:"
+		);
+	}
 }
 
 async function newhabitStage(update) {
@@ -90,6 +99,98 @@ async function newhabitStage(update) {
 		"INSERT INTO habits (activity, date_added) VALUES (?, now())",
 		[getMessageText(update)]
 	);
+}
+
+async function setnewreminderStage(update) {
+	let res = false;
+	if (session.target === undefined) {
+		res = await setnewreminderStage1(update);
+	} else if (session.hours === undefined) {
+		res = await setnewreminderStage2(update);
+	} else if (session.perXday === undefined) {
+		res = await setnewreminderStage3(update);
+		if (res == false) return;
+		
+		await pool.promise().execute(
+			"INSERT INTO reminders (target, hour, perXday) VALUES (?, ?, ?)",
+			[session.target, session.hours, session.perXday]
+		);
+		await sendMessage(
+			getSender(update).id,
+			`Reminder for ${session.target} fully set ` +
+			`at ${session.hours} each ${session.perXday} day.`
+		);
+		session = { stage: 'menu' };
+	}
+
+	return res;
+}
+
+async function setnewreminderStage1(update) {
+	if (isNaN(Number(getMessageText(update)))) {
+		await sendMessage(
+			getSender(update).id,
+			"Habit ID invalid! Resend them."
+		);
+		return false;
+	}
+
+	let habitId = Number(getMessageText(update));
+	let [rows] = await pool.promise().query(
+		"SELECT activity FROM habits WHERE id=?", [habitId]
+	);
+
+	if (!(rows?.length)) {
+		await sendMessage(
+			getSender(update).id,
+			"Habit ID not found! Resend them."
+		);
+		return false;
+	}
+
+	session.target = habitId;
+	await sendMessage(
+		getSender(update).id,
+		"Target habit set. Now send your reminder hour:"
+	);
+	return true;
+}
+
+async function setnewreminderStage2(update) {
+	let [hour, minute, second] = getMessageText(update)
+					.split(':')
+					.map(x => Number(x));
+	second = second ?? 0;
+	if (isNaN(hour) || isNaN(minute) || isNaN(second)) {
+		await sendMessage(
+			getSender(update).id,
+			"Reminder hour format invalid! Resend them as HH:MM:SS."
+		);
+		return false;
+	}
+
+	const q = x => String(x).padStart(2, '0');
+	session.hours = `${q(hour)}:${q(minute)}:${q(second)}`;
+	await sendMessage(
+		getSender(update).id,
+		"Reminder hour set! Now set your day frequency:"
+	);
+	return true;
+}
+
+async function setnewreminderStage3(update) {
+	let perXday = Number(getMessageText(update));
+
+	if (isNaN(perXday)) {
+		await sendMessage(
+			getSender(update).id,
+			"Reminder day invalid! Resend them as a number."
+		);
+		return false;
+	}
+
+	session.perXday = perXday;
+	return true;
 }
 
 app.listen(process.env.MYRMD_NET_PORT || 8080, () => {
